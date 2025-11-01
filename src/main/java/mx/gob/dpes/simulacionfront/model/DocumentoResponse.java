02727503020aef38e9e3a63bf36ac2e7dd861134
Hector Martinez Espinosa <mtzehector@gmail.com>
2025-10-31 21:35:30 -0600

    Actualizaci├│n inicial del proyecto

--- src/main/java/mx/gob/dpes/simulacionfront/model/DocumentoResponse.java
+++
@@ -1,9 +0,0 @@
-package mx.gob.dpes.simulacionfront.model;
-
-import lombok.Data;
-import mx.gob.imss.dpes.interfaces.documento.model.Documento;
-
-@Data
-public class DocumentoResponse extends Documento {
-  private String descTipoDocumento;
-}
